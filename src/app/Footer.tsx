f1d3fce84608f9a4214eddc9ffa5258b4318be02
Alienah <aidalbla@gmail.com>
2026-03-30 01:44:34 +0200

    add logo to Footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,8 +1,14 @@
+import logoCircle from "./logo-circle.png";
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <footer className="text-text-muted border-t border-border-soft">
-      <div className="mx-auto max-w-1200 px-24 py-24 text-helper">
-        PlayVerse
+      <div className="mx-auto max-w-1200 px-24 py-24">
+        <div className="flex items-center gap-8 text-helper">
+          <Image src={logoCircle} alt="" className="h-40 w-40" />
+          PlayVerse
+        </div>
       </div>
     </footer>
   );
